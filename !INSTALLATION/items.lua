b860aca883d8633794cd101517ebc7e31e024d1a
hightechy0 <highqez@gmail.com>
2024-06-10 16:23:30 +0530

    Initial commit

---
+++ !INSTALLATION/items.lua
@@ -0,0 +1,64 @@
+['medicalbag'] = {
+    label = 'Medical Bag',
+    weight = 220,
+    stack = true,
+    description = "A comprehensive medical kit for treating injuries and ailments.",
+},
+
+['bandage'] = {
+    label = 'Bandage',
+    weight = 100,
+    stack = true,
+    description = "A simple bandage used to cover and protect wounds.",
+},
+
+['defibrillator'] = {
+    label = 'Defibrillator',
+    weight = 100,
+    stack = true,
+    description = "Used for reviving patients.",
+},
+
+['tweezers'] = {
+    label = 'Tweezers',
+    weight = 100,
+    stack = true,
+    description = "Precision tweezers for safely removing foreign objects, such as bullets, from wounds.",
+},
+
+['burncream'] = {
+    label = 'Burn Cream',
+    weight = 100,
+    stack = true,
+    description = "Specialized cream for treating and soothing minor burns and skin irritations.",
+},
+
+['suturekit'] = {
+    label = 'Suture Kit',
+    weight = 100,
+    stack = true,
+    description = "A kit containing surgical tools and materials for stitching and closing wounds.",
+},
+
+['icepack'] = {
+    label = 'Ice Pack',
+    weight = 200,
+    stack = true,
+    description = "An ice pack used to reduce swelling and provide relief from pain and inflammation.",
+},
+
+['stretcher'] = {
+    label = 'Ice Pack',
+    weight = 200,
+    stack = true,
+    description = "An ice pack used to reduce swelling and provide relief from pain and inflammation.",
+},
+
+['emstablet'] = {
+    label = 'Ems tablet',
+    weight = 200,
+    stack = true,
+    client = {
+        export = 'ars_ambulancejob.openDistressCalls'
+    }
+},
